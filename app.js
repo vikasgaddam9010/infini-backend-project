28a3ac73ab2d6fd178bd31d98d44f8ba67ec3283
vikasgaddam9010 <vikasgaddam9010@gmail.com>
2024-08-13 10:04:25 +0000

    third commit

--- app.js
+++ app.js
@@ -25,6 +25,27 @@ const serverStart = async () => {
 
 serverStart()
 
+const check = (req, res, next) => {
+  const jwt = req.headers.authorization.split(' ')[1]
+  console.log(jwt)
+
+  if (jwt === undefined) {
+    res.status(401)
+    res.send('Invalid JWT Token')
+  } else {
+    jwttoken.verify(jwt, 'encryptedKey', async (err, payload) => {
+      if (err) {
+        res.status(401)
+        res.send('Invalid JWT Token')
+      } else {
+        req.username = payload.username
+        console.log(payload.username)
+        next()
+      }
+    })
+  }
+}
+
 app.post('/register/', async (req, res) => {
   const {username, password} = req.body
 
@@ -43,13 +64,13 @@ app.post('/register/', async (req, res) => {
   }
 })
 
+//API - 1
 app.post('/log-in/', async (req, res) => {
   const {username, password} = req.body
-  console.log(username)
   const dbRes = await dataBase.get(
     `SELECT * FROM users WHERE username = '${username}';`,
   )
-  
+
   if (dbRes === undefined) {
     res.status(400).send({message: 'Invalid user'})
   } else {
@@ -61,6 +82,21 @@ app.post('/log-in/', async (req, res) => {
       res.status(400).send({message: 'Invalid Password'})
     }
   }
+})
+
+//API - 2
+app.get('/get-events/', check, async (req, res) => {
+  const dbRes = await dataBase.all(`SELECT * FROM user_uploaded;`)
+  res.status(200).send(dbRes)
+})
+
+//API - 3
+app.post('/add-events/', check, async (req, res) => {
+  const {eventId, eventTitle, fileUrls} = req.body
+  const dbRes = await dataBase.run(
+    `INSERT INTO user_uploaded (event_id, username, event_title, uploades) VALUES ('${eventId}', '${req.username}', '${eventTitle}', '${fileUrls}');`,
+  )
+  res.status(200).send({message: 'Event Details Added...'})
 })
 
 app.listen(3000)
